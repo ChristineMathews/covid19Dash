6f0ef4a4ac87146be96d312ddda494493d4657f2
Christine Mathews <christinemathews@Christines-MacBook-Air.local>
2020-06-05 21:55:00 +0530

    api call function added

--- src/App.js
+++ src/App.js
@@ -1,1 +1,24 @@
 import React from 'react';
+
+
+import { Cards, CountryPicker, Chart } from './components';
+import { fetchData } from './api/'
+
+class App extends React.Component {
+    async componentDidMount() {
+        const data = await fetchData();
+    
+        this.setState({ data });
+      }
+    render() {
+      return (
+        <div className={styles.container}>
+          <img className={styles.image} src={image} alt="COVID-19" />
+          <Cards data={data} />
+          <CountryPicker handleCountryChange={this.handleCountryChange} />
+          <Chart data={data} country={country} /> 
+        </div>
+      );
+    }
+} 
+export default App;
